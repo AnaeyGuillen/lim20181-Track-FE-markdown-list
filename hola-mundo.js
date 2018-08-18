ebc10ff176f2e4fce9051b0065b6e22b22e74b19
AnaeyGuillen <anaey_gu@hotmail.com>
2018-08-17 19:55:14 -0500

    Modificando carpeta

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,18 +1,32 @@
-function add(x1, x2){
-    return x1+x2;
-}
-function substract(x1,x2) {
-    return x1-x2;
-}
-function multiply(x1,x2) {
-    return x1*x2;
-}
-function divide(x1,x2){
-    if(x2== 0){
-        console.log('NO SE PUEDE DIVIDIR POR 0');
-    } else{
-        return x1/x2;
-    }
-}
+let fs = require('fs');
+let path = require('path');
 
-console.log(divide(1,1));
+let searchRecursive = (dir, pattern)=>{
+  // This is where we store pattern matches of all files inside the directory
+  let results = [];
+
+  // Lee el contenido del directorio-carpeta
+  fs.readdirSync(dir).forEach((dirInner)=> {
+    // Obtiene el paquete absoluto
+    dirInner = path.resolve(dir, dirInner);
+
+    // Define si el archivo es carpeta o archivo
+    let stat = fs.statSync(dirInner);
+
+    // si el paquete es dentro de una carpeta, escanea y combina en results
+    if (stat.isDirectory()) {
+      results = results.concat(searchRecursive(dirInner, pattern));
+    }
+
+    // Si el archivo encuentra el patrón enviarlo dentro de results
+    if (stat.isFile() && dirInner.endsWith(pattern)) {
+      results.push(dirInner);
+    }
+  });
+
+  return results;
+};
+
+let files = searchRecursive('./', '.md'); //Establecer la terminación a buscar
+
+console.log(files);
