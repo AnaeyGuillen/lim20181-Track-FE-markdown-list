5562085e30144165611a88faf9a076cf26250bef
AnaeyGuillen <anaey_gu@hotmail.com>
2018-08-20 18:37:02 -0500

    Probando busqueda

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-#!/usr/bin/env node
+let fs = require('fs');
+let path = require('path');
 
-// const [,, ...args]=process.argv
+let searchRecursive = (dir, pattern)=>{
+  // Aquí es donde almacenamos coincidencias de patrones de todos los archivos dentro del directorio
+  let results = [];
 
-// console.log(args)
+  // Lee el contenido del directorio-carpeta
+  fs.readdirSync(dir).forEach((dirInner)=> {
+    // Obtiene el paquete absoluto
+    dirInner = path.resolve(dir, dirInner);
 
+    // Define si el archivo es carpeta o archivo
+    let stat = fs.statSync(dirInner);
+
+    // si el paquete es dentro de una carpeta, escanea y combina en results
+    if (stat.isDirectory()) {
+      results = results.concat(searchRecursive(dirInner, pattern));
+    }
+
+    // if(){
+
+    // }
+
+    // Si el archivo encuentra el patrón enviarlo dentro de results
+    if (stat.isFile() && dirInner.endsWith(pattern)) {
+      results.push(dirInner);
+    }
+  });
+
+  return results;
+};
+
+let files = searchRecursive('./', '.md'); //Establecer la terminación a buscar
+
+console.log(files);
